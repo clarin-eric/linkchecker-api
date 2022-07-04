c3c08250ac572be2d558e9d8c0766395491f5d3b
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2022-07-04 15:30:51 +0200

    base Spring boot structure

---
+++ src/main/java/eu/clarin/cmdi/linkcheckerweb/LinkcheckerWebApplication.java
@@ -0,0 +1,13 @@
+package eu.clarin.cmdi.linkcheckerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkcheckerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkcheckerWebApplication.class, args);
+	}
+
+}
