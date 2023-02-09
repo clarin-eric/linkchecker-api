7a1c6be6eee19b47807012c66151bd27b6474bd0
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2023-02-09 21:34:28 +0100

    reviewing package scans

--- src/main/java/eu/clarin/linkchecker/web/LinkcheckerWebApplication.java
+++ src/main/java/eu/clarin/linkchecker/web/LinkcheckerWebApplication.java
@@ -1,13 +1,14 @@
 package eu.clarin.linkchecker.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"eu.clarin.cmdi.cpa", "eu.clarin.cmdi.linkcheckerweb"})
-@EnableJpaRepositories(basePackages = "eu.clarin.cmdi.cpa.repository")
-@EntityScan(basePackages = "eu.clarin.cmdi.cpa.model")
+@SpringBootApplication(scanBasePackages = {"eu.clarin.linkchecker.web", "eu.clarin.linkchecker.persistence"})
+@EnableJpaRepositories(basePackages = "eu.clarin.linkchecker.persistence.repository")
+@EntityScan(basePackages = "eu.clarin.linkchecker.persistence.model")
 public class LinkcheckerWebApplication {
 
 	public static void main(String[] args) {
